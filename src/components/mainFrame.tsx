356884539c6321493d4bfe106bd33cfaef54aaf7
Max Fan (Shang Hai Wei Chuang Ruan Jian) <v-tiafan@microsoft.com>
2018-03-01 11:43:54 +0800

    Fix back track at index

--- src/components/mainFrame.tsx
+++ src/components/mainFrame.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { RoutePath } from '../routing';
 
 export default class MainFrame extends React.Component {
     render() {
@@ -7,14 +6,14 @@ export default class MainFrame extends React.Component {
             <div id="main" className="container-fluid p-0">
                 <div id="header" className="container-fluid text-center">
                     <nav className="nav" >
-                        <a className="nav-link active" href="">Home</a>
+                        <a className="nav-link active" href="/#">Home</a>
                     </nav>
                 </div>
                 {this.props.children}
                 <div id="footer" className="container-fluid text-center">
                     Host on <a href="https://pages.github.com/">GitHub Pages</a>
                 </div>
-            </div>
+            </div >
         );
     }
 }
